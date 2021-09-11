5d3515d8da871056c5a5178b16737726b86212af
Joe.Wang <sdauwangzh@163.com>
2021-09-11 10:15:25 +0800

    添加cli

---
+++ flo_cli/cmd/build.go
@@ -0,0 +1,20 @@
+/*
+ * @Author: Joe<sdauwangzh@163.com>
+ * @Date: 2021-09-11 10:11:27
+ * @Description:编译
+ * @FilePath: /flo_cli/cmd/build.go
+ * JoeSay: What does not kill me, makes me stronger
+ */
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	buildCmd = &cobra.Command{
+		Use:   "build",
+		Short: "TODO",
+		Long:  `TODO`,
+	}
+)
---
+++ flo_cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/HelloWorldImW/flo/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
